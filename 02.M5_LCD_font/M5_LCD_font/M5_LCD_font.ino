c89b5e384da7227b02fe10308903dcf6d701a997
catalyst-yuki-k <catalyst-yuki_k@hotmail.com>
2022-09-25 23:05:07 +0900

    update

--- 02.M5_LCD_font/M5_LCD_font/M5_LCD_font.ino
+++ 02.M5_LCD_font/M5_LCD_font/M5_LCD_font.ino
@@ -6,13 +6,21 @@ void setup() {
     //バックライトの明るさを0～255で指定
     M5.Lcd.setBrightness(255);
 
+    //文字色を設定（文字背景色は透明）
+    M5.Lcd.setTextColor(WHITE);
+
     //文字の大きさを1～7で指定
-    M5.Lcd.setTextSize(4);
+    M5.Lcd.setTextSize(1);
 
     //文字色を設定（文字背景色は透明）
-    M5.Lcd.setTextColor(BLUE);
+    M5.Lcd.setTextColor(BLACK,WHITE);
+    //文字の表示
+    M5.Lcd.print("Hello ");
 
-    M5.Lcd.printf("Hello world!");
+    //文字色を設定（文字背景色は透明）
+    M5.Lcd.setTextColor(WHITE,BLACK);
+    //文字の表示(改行)
+    M5.Lcd.println("World!");
 }
 
 void loop() {
